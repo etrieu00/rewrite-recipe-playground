f4524b4954151ad9d771c9f6ab308284caa3ad46
Eric Trieu <atlas@Collapsed.local>
2024-09-02 18:31:01 -0400

    remove annotation

--- src/main/java/com/etrieu00/demo/controller/DemoController.java
+++ src/main/java/com/etrieu00/demo/controller/DemoController.java
@@ -1,6 +1,5 @@
 package com.etrieu00.demo.controller;
 
-import com.etrieu00.logging.annotation.AuditEndpoint;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,7 +9,6 @@ public class DemoController {
     public record Person(String name, Integer age) {
     }
 
-    @AuditEndpoint
     @GetMapping("/person")
     public Person retrievePerson() {
         return new Person("Eric", 27);
